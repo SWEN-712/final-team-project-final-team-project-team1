3338a0fbbf42ce8e91033919db45cb70b27eb348
Kirtana <ks3057@rit.edu>
2020-04-27 16:12:39 -0400

    disabling object detection

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,21 +1,20 @@
 import os
 from flask import Flask, render_template, request
 
-# import OCR and Translation modules
 import ocr_core
 import translator
-import object_detect
+# import object_detect
 
-# define a folder to store and later serve the images
+# upload and object detection output folders
 UPLOAD_FOLDER = 'static/uploads/'
 OUTPUT_FOLDER = 'static/output/'
 
-# allow files of a specific type
+# configure the folders to the app
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
-# route and function to handle the home page
+# home page routing
 @app.route('/')
 def home_page():
     return render_template('index.html')
@@ -45,7 +44,7 @@ def upload_page():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
             lang = ocr_core.format_language(lang)
             extracted_text = ocr_core.ocr_core(file, lang)
-            objects_detected = object_detect.object_detect(file.filename)
+            # objects_detected = object_detect.object_detect(file.filename)
             if target_lang != 'None':
                 target_lang = translator.format_language(target_lang)
                 extracted_text = translator.translate(extracted_text, target_lang)
@@ -53,6 +52,20 @@ def upload_page():
             just_fname = file.filename.split(".")[0]
 
             # extract the text and display it
+            # return render_template('upload.html',
+            #                        msg='Successfully processed...',
+            #                        extracted_text=extracted_text,
+            #                        source_languages=ocr_core.SOURCE_LANGUAGE_OPTIONS,
+            #                        target_languages=translator.TARGET_LANGUAGE_OPTIONS,
+            #                        img_src=os.path.join(app.config[
+            #                                                 'UPLOAD_FOLDER'],
+            #                                             file.filename),
+            #                        obj_det=os.path.join(app.config[
+            #                                                 'OUTPUT_FOLDER'],
+            #                                             just_fname + '.png'),
+            #                        objects_detected = objects_detected
+            #                        )
+
             return render_template('upload.html',
                                    msg='Successfully processed...',
                                    extracted_text=extracted_text,
@@ -61,10 +74,6 @@ def upload_page():
                                    img_src=os.path.join(app.config[
                                                             'UPLOAD_FOLDER'],
                                                         file.filename),
-                                   obj_det=os.path.join(app.config[
-                                                            'OUTPUT_FOLDER'],
-                                                        just_fname + '.png'),
-                                   objects_detected = objects_detected
                                    )
     elif request.method == 'GET':
         return render_template('upload.html', source_languages=ocr_core.SOURCE_LANGUAGE_OPTIONS,
